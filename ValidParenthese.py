889f09dec845cdbc714ec3b8c3be70778024bd3f
Arshad0786 <js07150715@gmail.com>
2020-04-27 12:07:04 +0800

    Finish first version with bad time complexity but accepted by leetcode

--- ValidParenthese.py
+++ ValidParenthese.py
@@ -1,4 +1,17 @@
 class Solution:
-    def isValid(self,str):
-        UpperToLower = {"{":"}","[":"]","(":")"}
-    pass
+    def isValid(self, input):
+        UpperToLower = {"{": "}", "[": "]", "(": ")",
+                        "}": None, "]": None, ")": None}
+        i = 0
+        loops = len(input) / 2 + 1
+        while (i < loops):
+            index = 0
+            while (index < len(input)-1):
+                if(UpperToLower[input[index]] == input[index+1]):
+                    input = input[0:index] + input[index+2:len(input)]
+                index = index + 1
+            i = i + 1
+        if input:
+            return False
+        else:
+            return True
